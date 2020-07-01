54860b716824ed3c28872371ffcaee1511cbc782
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-07-01 20:57:27 +0200

    added funcion

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-console.log("Witam serdecznie");
-
-let przycisk = document.querySelector(".advertisement");
-let pic = document.querySelector(".image__pic")
-
-przycisk.addEventListener("click", () => {
-    pic.remove();
-    przycisk.innerText = "Reklama zniknęła :)";
-
-})
-
-
-
-let like = document.querySelector(".like");
-
-like.addEventListener("click", () => {
-
-    if (like.innerText === "Daj lajka") {
-        like.innerText = "Dziękuje";
-    } else {
-        like.innerText = "Daj lajka";
+{
+    const welcome = () => {
+        console.log("Witam serdecznie");
     }
+    
+    const remove = () =>  {
+        const pic = document.querySelector(".image__pic");
+        const przycisk = document.querySelector(".advertisement");
+        pic.remove();
+        przycisk.innerText = "Reklama zniknęła :)";
+        
+    };
 
-
-})
+    const init = () =>{
+        const przycisk = document.querySelector(".advertisement");
+        przycisk.addEventListener("click", remove)
+        welcome();
+       };
+        
+    init ();
+    
+ 
+    {
+        const changebuttonlike = () => {
+            if (like.innerText === "Daj lajka") {
+                like.innerText = "Dziękuje";
+            } else {
+                like.innerText = "Daj lajka";
+            }
+        };
+        const like = document.querySelector(".like");
+        like.addEventListener("click", changebuttonlike);
+       
+    }
+    
+   
+}
